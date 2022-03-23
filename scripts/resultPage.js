cd1f8b4416e30d43824bc637fdb0ec83c87c3601
Jageron <eshik2002@gmail.com>
2022-03-23 14:24:48 +0300

    added final page

--- scripts/resultPage.js
+++ scripts/resultPage.js
@@ -1,13 +1,26 @@
 window.onload = function(){
     ans = sessionStorage.getItem('answers');
-    res = []
+    res = {}
+    maxId = 0;
+    maxNum = 0;
     for (let i = 0; i < ans.length; i++) {
         if(res[ans[i]]!=undefined){
             res[ans[i]]++;
+            if(res[ans[i]]>maxNum){
+                maxNum = res[ans[i]];
+                maxId = ans[i];
+            }
         }else{
             res[ans[i]]=1;
         }
         
     }
-    console.log(ans);
+    fetch('JSON/places.json')
+    .then(response => response.json())
+    .then(function(places){
+        document.querySelector(".placeName").innerHTML = places[maxId]['name'];
+        document.querySelector(".placeDiscription").innerHTML = places[maxId]['discription'];
+        document.querySelector(".placeAdress").innerHTML = places[maxId]['adress'];
+    });
+    
 }
